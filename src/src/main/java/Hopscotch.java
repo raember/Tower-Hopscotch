f3618e86be67683564cfef85f8f021d42f529dbb
emberrap <emberrap@students.zhaw.ch>
2017-10-03 12:31:32 +0200

    Added IntelliJ project template including Maven

---
+++ src/src/main/java/Hopscotch.java
@@ -0,0 +1,11 @@
+import com.sun.media.jfxmedia.logging.Logger;
+
+/**
+ * Created by Raphael Emberger on 03.10.2017.
+ */
+public class Hopscotch {
+    public static void main(String args[]) {
+        Logger.setLevel(Logger.INFO);
+        Logger.logMsg(Logger.INFO, "\nHello world!");
+    }
+}
