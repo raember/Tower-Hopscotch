aec62c46a844f9c0bec6d85ace66337894dc89f8
Nicolas Eckhart <nicolaseckhart@hotmail.com>
2017-10-16 08:21:31 +0200

    renamed src folder and idea project to towerhopscotch

--- towerhopscotch/src/main/java/Hopscotch.java
+++ towerhopscotch/src/main/java/Hopscotch.java
@@ -1,8 +1,5 @@
 import com.sun.media.jfxmedia.logging.Logger;
 
-/**
- * Created by Raphael Emberger on 03.10.2017.
- */
 public class Hopscotch {
     public static void main(String args[]) {
         Logger.setLevel(Logger.INFO);
