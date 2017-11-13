f2ac0eada68fff4f5f14d33ff8dbcf1f55140dee
emberrap <emberrap@students.zhaw.ch>
2017-11-13 23:34:20 +0100

    Changed Classdiagram and started implementing Tower specific classes

---
+++ towerhopscotch/src/main/java/ch/zhaw/psit/towerhopscotch/tower/MonoTower.java
@@ -0,0 +1,12 @@
+package ch.zhaw.psit.towerhopscotch.tower;
+
+import java.awt.*;
+
+/**
+ * Created by Raphael Emberger on 13.11.2017.
+ */
+public class MonoTower extends Tower {
+    protected MonoTower(int price, Color color) {
+        super(price, color);
+    }
+}
